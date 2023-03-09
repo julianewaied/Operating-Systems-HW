73dca4466929fc6af61f370ee358271efd3bb617
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-03-10 01:31:31 +0200

    Modularing code and fixing bugs

--- HW1/myshell.c
+++ HW1/myshell.c
@@ -19,14 +19,16 @@ typedef struct op
 
 void get_args(char* argv[MAX_ARGS],char* command);
 node* list_push(node*,node*);
-nodeptr cmd_push(node* new_node, node* head, char* cmd);
+nodeptr cmd_push(node* head, char* cmd);
 void print_list(nodeptr current);
+int is_background(char* argv[MAX_ARGS]);
+void activate(char* command);
+
 int main(void)
 {
     close(2);
     dup(1);
     char command[BUFFER_SIZE];
-    // a linked list of commands proceeded
     node* head = NULL;
 
     while (1)
@@ -35,67 +37,24 @@ int main(void)
         memset(command, 0, BUFFER_SIZE);
         fgets(command, BUFFER_SIZE, stdin);
         if(strncmp(command, "exit", 4) == 0)
-        {
             break;
-        }
-        if(strncmp(command, "history", 7) == 0)
-        {
-            print_list(head);
-            continue;
-        }
-        
         //remove the \n from command
         command[strlen(command)-1]='\0';
-        
-        // dont remove this code, waiting for safaa answer
-/*
-        int background_task=(command[strlen(command)-1]=='&');
+        head = cmd_push(head,command);
+        if(strncmp(command, "history", 7) == 0)
+            print_list(head);
+        else activate(command);
 
-        //remove the '&' token if we have a background task
-        if(background_task) command[strlen(command)-1] = '\0';
-        
-*/
-        nodeptr newNode = (nodeptr)malloc(sizeof(node));
-        if(newNode==NULL) exit(1);
-        newNode->next=NULL;
-        newNode->cmd = malloc(BUFFER_SIZE * sizeof(int));
-        if(newNode->cmd==NULL) exit(1);
-        head = cmd_push(newNode,head,command);
-        
-        
-
-        char* argv[MAX_ARGS];
-        get_args(argv,command);
-
-        int background_task;
-        int i=0;
-        while(argv[i+1]!=NULL) i++;
-        // print(argv);
-        char* last = argv[i];
-        background_task= last[strlen(last)-1]=='&';
-
-        //remove the '&' token if we have a background task
-        if(background_task) last[strlen(last)-1]='\0';
-        // if it's spaced from other arguments, get rid of it.
-        if(last[0]=='\0') argv[i] = NULL;
-
-        int pid = fork();
-
-        if(pid==0){
-            execvp(argv[0], argv);
-            fprintf(stdout,"%s","command execution failed!\n");
-            exit(1);
-        }
-
-        if(!background_task)
-            waitpid(pid,NULL,0);
-
-        
     }
     return 0;
 }
-nodeptr cmd_push(node* new_node, node* head, char* cmd)
+nodeptr cmd_push(node* head, char* cmd)
 {
+    nodeptr new_node = (nodeptr)malloc(sizeof(node));
+    if(new_node==NULL) exit(1);
+    new_node->next=NULL;
+    new_node->cmd = malloc(BUFFER_SIZE * sizeof(int));
+    if(new_node->cmd==NULL) exit(1);
     nodeptr h = list_push(new_node,head);
     strcpy(h->cmd,cmd);
     return h;
@@ -113,7 +72,6 @@ void print_list(nodeptr curr)
     fprintf(stdout, "%s\n", curr->cmd);
     print_list(curr->next);
 }
-
 void get_args(char* argv[MAX_ARGS],char* const command){
     char* arg;
     arg=strtok(command," ");
@@ -125,4 +83,32 @@ void get_args(char* argv[MAX_ARGS],char* const command){
     }
     argv[i]=NULL;
 
+}
+void activate(char* command)
+{
+    char* argv[MAX_ARGS];
+    get_args(argv,command);
+    int background_task = is_background(argv);
+    pid_t pid = fork();
+    if(pid==0)
+    {
+        execvp(argv[0], argv);
+        perror("error");
+        exit(1);
+    }
+    if(!background_task)
+        waitpid(pid,NULL,0);
+}
+int is_background(char* argv[MAX_ARGS])
+{
+    int i=0;
+    int background_task;
+    while(argv[i+1]!=NULL) i++;
+    char* last = argv[i];
+    background_task= last[strlen(last)-1]=='&';
+    //remove the '&' token if we have a background task
+    if(background_task) last[strlen(last)-1]='\0';
+    // if it's spaced from other arguments, get rid of it.
+    if(last[0]=='\0') argv[i] = NULL;
+    return background_task;
 }
