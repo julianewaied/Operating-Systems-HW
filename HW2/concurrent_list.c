f7be0a69451dca80525c93a756d083f097980d3a
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-04-06 00:13:00 +0300

    fixing the locks shit

--- HW2/concurrent_list.c
+++ HW2/concurrent_list.c
@@ -3,108 +3,93 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "concurrent_list.h"
-int* list_arr;
+int *list_arr;
 int in_read;
 int out_read;
-struct node {
+struct node
+{
+  // make it double linked list to make implementation easier :)
   int value;
-  node* next;
-  node* previous;
+  node *next;
+  node *previous;
+  pthread_mutex_t lock;
   // add more fields
 };
 
-struct list {
-  // add fields
-  node* head;
-  int size;
+struct list
+{
+  // we need the lock for reading the head.
+  node *head;
+  pthread_mutex_t lock;
 };
 
-void print_node(node* node)
+void print_node(node *node)
 {
   // DO NOT DELETE
-  if(node)
+  if (node)
   {
     printf("%d ", node->value);
   }
 }
 
-list* create_list()
+list *create_list()
 {
-  // add code here
-  list* l = malloc(sizeof(list))
+  list *l = malloc(sizeof(list))
   l->head = NULL;
-  return l; // REPLACE WITH YOUR OWN CODE
+  return l;
 }
 
-void delete_list(list* list)
+void delete_list(list *list)
 {
   // add code here
 }
 
-void insert_value(list* list, int value)
+void insert_value(list *list, int value)
 {
   // add code here
 }
 
-void remove_value(list* list, int value)
+void remove_value(list *list, int value)
 {
   // add code here
 }
 
-void* scan(void* argv)
+void print_list(list *list)
 {
-  node* current = (node*) argv;
-  while(current)
+  if(!list) exit(1);
+  pthread_mutex_lock(list->lock);
+  node *current = list->head;
+  pthread_mutex_unlock(list->lock);
+  int value;
+  while (current)
   {
-    // bounded buffer algorithm
-    while (in_read+1==out_read);
-    int value = current->value;
-    
-    list_arr[in_read] = value;
-    current = current->next;
-    in_read++;
-  }
-}
-
-void print_list(list* list)
-{
-  // implementing bounded buffer without cyclic arrays
-  node* current = list->head;
-  list_arr = malloc((list->size)*sizeof(int));
-  in_read =0;
-  out_read = 0;
-  pthread_t* scanner;
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-  pthread_create(scanner,&attr,scan,(void*)current);
-  int i = 0 ;
-  for(i=0;i<list->size;i++)
-  {
-    while(in_read == out_read);                         // bounded buffer
-    fprintf(stdout,"%d ", list_arr[out_read]);
-    out_read = (out_read + 1);
+    pthread_mutex_lock(current->lock);
+    value = current->value;
+    next = current->next;
+    pthread_mutex_unlock(current->lock);
+    fprintf(stdout, "%d ", value);
+    current = next;
   }
   printf("\n"); // DO NOT DELETE
-  free(list_arr)
 }
 
-void count_list(list* list, int (*predicate)(int))
+void count_list(list *list, int (*predicate)(int))
 {
-  // notice! this is a read only task, meaning, locks are not needed.
+  if(!list) exit(1);
   int count = 0; // DO NOT DELETE
+  pthread_mutex_lock(list->lock);
   node* current = list->head;
-  pthread_t* threads = malloc(sizeof(pthread_t)*list->size);
-  int i=0;
-  while(current)
+  pthread_mutex_unlock(list->lock);
+  node *next = NULL;
+  int value;
+  while (current)
   {
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_create(threads+i,&attr,predicate,current->value);
-    i++;
-  }
-  for(i=0;i<list->size;i++)
-  {
-    count += pthread_join(threads[i],NULL);
+    pthread_mutex_lock(current->lock);
+    value = current->value;
+    next = current->next;
+    pthread_mutex_unlock(current->lock);
+    current = next;
+    count += predicate(value);
   }
   printf("%d items were counted\n", count); // DO NOT DELETE
   free(threads)
